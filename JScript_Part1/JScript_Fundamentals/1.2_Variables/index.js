f5ec05558db4315651975ebe922aafe3646ec5c6
Aswin <tve21cs037@cet.ac.in>
2025-08-13 10:49:00 +0530

    initial

---
+++ JScript_Part1/JScript_Fundamentals/1.2_Variables/index.js
@@ -0,0 +1,9 @@
+//task1
+let admin,name;
+name="John";
+admin=name;
+alert(admin);
+
+//taskk2
+let ourPlanet="Earth";
+let currentVisitor="John";
---
+++ JScript_Part1/JScript_Fundamentals/1.4_Interaction/index.js
@@ -0,0 +1,5 @@
+let title="What is your name?"
+const result=prompt(title);
+alert(result);
+// const isBoss=confirm("Are u the boss?");
+// alert(isBoss);
