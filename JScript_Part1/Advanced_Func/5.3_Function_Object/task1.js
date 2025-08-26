138f9cc0e9259aa539949bcf493b9f82183ba985
AswinKrishnan2125 <aswinkrishnanr04@gmail.com>
2025-08-26 15:01:05 +0530

    function-object

---
+++ JScript_Part1/Advanced_Func/5.3_Function_Object/task1.js
@@ -0,0 +1,23 @@
+function makeCounter() {
+    let count = 0;
+    
+    function counter(){
+        return  count++;
+    }
+    counter.set = ((x)=> count=x);
+    counter.decrease = (x) => count--;
+    return counter;
+  }
+  
+  let counter = makeCounter();
+  
+  console.log( counter() ); // 0
+  console.log( counter() ); // 1
+  
+  counter.set(10); // set the new count
+  
+  console.log( counter() ); // 10
+  
+  counter.decrease(); // decrease the count by 1
+  
+  console.log( counter() );
